0d86324f714fccdd344a844539387e1c58b8072b
Nico Hauser <me@tyratox.ch>
2018-09-10 15:01:06 +0200

    Improved stripTags

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -1,7 +1,9 @@
+import decode from "decode-html";
+
 /**
  * Strips html tags from a string
  * @param {string} string The string to strip
  * @returns {string} The stripped string
  */
 export const stripTags = string =>
-  (string && string.replace(/<(?:.|\n)*?>/gm, "")) || "";
+  (string && decode(string).replace(/<(?:.|\n)*?>/gm, "")) || "";

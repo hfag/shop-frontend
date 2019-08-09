05c39773f21ffa2dec6f1a889a1a9d621c3e4096
Nico Hauser <me@tyratox.ch>
2019-08-09 17:18:16 +0200

    Update webpack-build.config.js

--- webpack-build.config.js
+++ webpack-build.config.js
@@ -231,7 +231,7 @@ module.exports = {
         ]
       },
       {
-        test: /\.(woff2?|eot|ttf|svg)$/,
+        test: /\.(png|jpe?g|gif|woff2?|eot|ttf|svg)$/,
         loader: "file-loader"
       }
     ]

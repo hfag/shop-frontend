e305baa284e4cfe9c4d3963c9e2ba917c6e52887
Nico Hauser <me@tyratox.ch>
2019-09-10 09:58:54 +0200

    Alternative language switcher design

--- src/components/header/LanguageSwitcher.jsx
+++ src/components/header/LanguageSwitcher.jsx
@@ -2,62 +2,47 @@ import React, { useMemo } from "react";
 import styled from "styled-components";
 import { withRouter } from "react-router";
 
-import Dropdown from "../Dropdown";
 import Link from "../Link";
-import Triangle from "../Triangle";
 import { getLanguageFromLocation } from "../../utilities/i18n";
-
-const LanguageSelection = styled(Dropdown)`
-  left: -1rem;
-  right: -1rem;
-`;
-
-const CurrentLanguage = styled.div``;
+import Flexbar from "../Flexbar";
 
 const LANGUAGES = {
   de: {
-    label: "Deutsch"
+    label: "DE"
   },
   fr: {
-    label: "Français"
+    label: "FR"
   }
 };
+
+const Language = styled.div`
+  margin-right: 0.5rem;
+`;
 
 const LanguageSwitcher = React.memo(({ dropdown, setDropdown, location }) => {
   const language = useMemo(() => getLanguageFromLocation(location), [location]);
 
   return (
     <React.Fragment>
-      <Link
-        onClick={() =>
-          setDropdown(dropdown === "language" ? false : "language")
-        }
-        negative
-        flex
-      >
-        <CurrentLanguage>{LANGUAGES[language].label}</CurrentLanguage>
-        <Triangle color="#fff" size="0.5rem" />
-      </Link>
-      {dropdown === "language" && (
-        <LanguageSelection>
-          {Object.keys(LANGUAGES)
-            .filter(languageKey => languageKey !== language)
-            .map(languageKey => (
-              <div key={languageKey}>
-                <Link
-                  onClick={() => {
-                    window.location.href = `/${languageKey}/`; /*`/${languageKey}/${location.pathname
+      <Flexbar>
+        {Object.keys(LANGUAGES).map(languageKey => (
+          <Language key={languageKey}>
+            <Link
+              active={languageKey === language}
+              styled={languageKey === language}
+              negative
+              onClick={() => {
+                window.location.href = `/${languageKey}/`; /*`/${languageKey}/${location.pathname
                     .split("/")
                     .slice(2)
                     .join("/")}`;*/
-                  }}
-                >
-                  {LANGUAGES[languageKey].label}
-                </Link>
-              </div>
-            ))}
-        </LanguageSelection>
-      )}
+              }}
+            >
+              {LANGUAGES[languageKey].label}
+            </Link>
+          </Language>
+        ))}
+      </Flexbar>
     </React.Fragment>
   );
 });

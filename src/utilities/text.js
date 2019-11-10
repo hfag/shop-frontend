cc28efc34aa193ffeaab5509040dcf197c7ddb46
Nico Hauser <me@tyratox.ch>
2019-11-10 14:52:38 +0100

    Improved html entity parsing

--- src/utilities/text.js
+++ src/utilities/text.js
@@ -1,18 +1,9 @@
-import { isClient } from "./ssr";
-
-const Parser = isClient ? window.DOMParser : require("xmldom").DOMParser;
+const Entities = require("html-entities").AllHtmlEntities;
+const entities = new Entities();
 
 /**
  * Decodes html entities
  * @param {string} string The string to decode
  * @returns {string} The decoded string
  */
-export const decodeHTMLEntities = string => {
-  const parser = new Parser();
-  const dom = parser.parseFromString(
-    "<!doctype html><body>" + string,
-    "text/html"
-  );
-
-  return dom.body.innerHTML;
-};
+export const decodeHTMLEntities = string => entities.decode(string);

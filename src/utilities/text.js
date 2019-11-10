03131f2987de5f21af22d1bbdb8bb6443e2bf506
Nico Hauser <me@tyratox.ch>
2019-11-10 14:31:35 +0100

    polyfilled DOMParser for ssr

--- src/utilities/text.js
+++ src/utilities/text.js
@@ -1,10 +1,14 @@
+import { isClient } from "./ssr";
+
+const Parser = isClient ? window.DOMParser : require("xmldom").DOMParser;
+
 /**
  * Decodes html entities
  * @param {string} string The string to decode
  * @returns {string} The decoded string
  */
 export const decodeHTMLEntities = string => {
-  const parser = new DOMParser();
+  const parser = new Parser();
   const dom = parser.parseFromString(
     "<!doctype html><body>" + string,
     "text/html"

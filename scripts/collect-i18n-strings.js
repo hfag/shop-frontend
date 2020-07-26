27dfd0279c83937734dab1a30772642cc142285a
Nico Hauser <me@tyratox.ch>
2020-07-26 20:23:36 +0200

    Updated message extraction

--- scripts/collect-i18n-strings.js
+++ scripts/collect-i18n-strings.js
@@ -1,33 +1,16 @@
 const fs = require("fs");
 
-const { sync: globSync } = require("glob");
 const { sync: mkdirpSync } = require("mkdirp");
 
-const filePattern = "./messages/**/*.json";
 const outputDir = "./locales/";
 
-// Aggregates the default messages that were extracted from the example app's
-// React components via the React Intl Babel plugin. An error will be thrown if
-// there are messages in different components that use the same `id`. The result
-// is a flat collection of `id: message` pairs for the app's default locale.
-let defaultMessages = globSync(filePattern)
-  .map((filename) => fs.readFileSync(filename, "utf8"))
-  .map((file) => JSON.parse(file))
-  .reduce((collection, descriptors) => {
-    descriptors.forEach(({ id, defaultMessage }) => {
-      if (collection.hasOwnProperty(id)) {
-        throw new Error(`Duplicate message id: ${id}`);
-      }
-      collection[id] = defaultMessage;
-    });
+const messages = JSON.parse(fs.readFileSync("./messages.json", "utf8"));
 
-    return collection;
-  }, {});
 // Create a new directory that we want to write the aggregate messages to
 mkdirpSync(outputDir);
 
 // Write the messages to this directory
 fs.writeFileSync(
   outputDir + "de.json",
-  `{ "de": ${JSON.stringify(defaultMessages, null, 2)} }`
+  `{ "de": ${JSON.stringify(messages, null, 2)} }`
 );

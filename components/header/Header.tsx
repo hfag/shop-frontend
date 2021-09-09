a93acaf3dc3b41581d0d7a59f92baa9b5752ec26
Nico Hauser <me@tyratox.ch>
2021-09-09 15:24:40 +0200

    add favicon

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -75,7 +75,6 @@ const HtmlHead: FunctionComponent<{ intl: IntlShape }> = React.memo(
       <title>{intl.formatMessage(shop.siteTitle)}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta name="author" content="Nico Hauser" />
       <meta name="format-detection" content="telephone=no" />
       <meta
         name="description"
@@ -84,6 +83,82 @@ const HtmlHead: FunctionComponent<{ intl: IntlShape }> = React.memo(
       <link rel="canonical" href={ABSOLUTE_URL} />
       <link rel="preconnect" href={API_BASE_URL} />
       <link rel="preconnect" href={WP_BLOG_URL} />
+      <link
+        rel="apple-touch-icon"
+        sizes="57x57"
+        href="/images/favicon/apple-icon-57x57.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="60x60"
+        href="/images/favicon/apple-icon-60x60.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="72x72"
+        href="/images/favicon/apple-icon-72x72.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="76x76"
+        href="/images/favicon/apple-icon-76x76.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="114x114"
+        href="/images/favicon/apple-icon-114x114.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="120x120"
+        href="/images/favicon/apple-icon-120x120.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="144x144"
+        href="/images/favicon/apple-icon-144x144.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="152x152"
+        href="/images/favicon/apple-icon-152x152.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/images/favicon/apple-icon-180x180.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="192x192"
+        href="/images/favicon/android-icon-192x192.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/images/favicon/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="96x96"
+        href="/images/favicon/favicon-96x96.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/images/favicon/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/images/favicon/manifest.json" />
+      <meta name="msapplication-TileColor" content="#ffffff" />
+      <meta
+        name="msapplication-TileImage"
+        content="/images/favicon/ms-icon-144x144.png"
+      />
+      <meta name="theme-color" content="#ffffff" />
     </Head>
   )
 );

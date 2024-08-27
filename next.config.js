d1d0c89d38f058e11555e4c22e2f78c1639d8665
Nico Hauser <me@tyratox.ch>
2024-08-27 17:59:08 +0200

    Undo config change

--- next.config.js
+++ next.config.js
@@ -7,16 +7,16 @@ const withTM = require("next-transpile-modules")([
 ]);
 
 const env = {
-  // REVALIDATION_SECRET: "test",
-  // ABSOLUTE_URL: "http://127.0.0.1:8080",
-  // ASSET_URL: "http://127.0.0.1:3000/assets",
-  // API_URL: "http://127.0.0.1:3000/shop-api",
-  // ADMIN_API_URL: "http://127.0.0.1:3000/admin-api",
-  ABSOLUTE_URL: "https://shop.feuerschutz.ch",
-  ASSET_URL: "https://shop.feuerschutz.ch/assets",
-  API_URL: "https://shop.feuerschutz.ch/shop-api",
-  ADMIN_API_URL: "https://shop.feuerschutz.ch/admin-api",
-  REVALIDATION_SECRET: "",
+  REVALIDATION_SECRET: "test",
+  ABSOLUTE_URL: "http://127.0.0.1:8080",
+  ASSET_URL: "http://127.0.0.1:3000/assets",
+  API_URL: "http://127.0.0.1:3000/shop-api",
+  ADMIN_API_URL: "http://127.0.0.1:3000/admin-api",
+  // ABSOLUTE_URL: "https://shop.feuerschutz.ch",
+  // ASSET_URL: "https://shop.feuerschutz.ch/assets",
+  // API_URL: "https://shop.feuerschutz.ch/shop-api",
+  // ADMIN_API_URL: "https://shop.feuerschutz.ch/admin-api",
+  // REVALIDATION_SECRET: "",
   WP_BLOG_URL: "https://api.feuerschutz.ch",
   PUBLIC_PATH: "/",
 };

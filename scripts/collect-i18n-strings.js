21d7cbb767097a89ff08d2065525c3e4039d62c4
Nico Hauser <me@tyratox.ch>
2025-12-10 10:02:05 +0100

    Remove babel

--- scripts/collect-i18n-strings.js
+++ scripts/collect-i18n-strings.js
@@ -7,7 +7,7 @@ const filePattern = "./messages/**/*.json";
 const outputDir = "./locales/";
 
 // Aggregates the default messages that were extracted from the example app's
-// React components via the React Intl Babel plugin. An error will be thrown if
+// React components via formatjs. An error will be thrown if
 // there are messages in different components that use the same `id`. The result
 // is a flat collection of `id: message` pairs for the app's default locale.
 let defaultMessages = globSync(filePattern)
